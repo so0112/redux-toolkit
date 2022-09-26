abdd41439a8b32ab5c3657110b2f5ae26d35a4f9
so0112 <tkddh0112@gmail.com>
2022-09-26 22:39:56 +0900

    Chore: 리덕스 툴킷 학습용 초기세팅

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div>
+      <button>+</button> 0
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Counter></Counter>
+    </div>
+  );
+}
